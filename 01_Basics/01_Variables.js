f63796f4ab287cb674d9390032d63c6e646f4c18
chirag tyagi <45567899+tyagichirag5@users.noreply.github.com>
2023-07-25 10:15:47 +0000

    Add variables file

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId=122344
+let accountEmail="chirag@gmail.com"
+var accountPassword="123456"
+accountCity="Panipat"
+
+// accountId=234. // not allowed
+
+let accountState;
+
+accountEmail="c@g.com"
+accountPassword="23322222"
+accountCity="Noida"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
